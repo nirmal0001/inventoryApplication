e55a2bc34418e8e112bedc17a7b7c800851c7ca9
nimma dev <kdjj18322@gmail.com>
2025-10-18 19:49:35 +0530

    completed genre homepage

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,10 @@
-const {getAllGenre } = require('../db/queries')
+const { getAllGenre } = require('../db/queries');
 
-exports.index = async (req, res) =>{
-  const data = await getAllGenre()
-  res.render('index', { title: 'Browse Genre', content: 'genre', data});
-}
+exports.index = async (req, res) => {
+  const data = await getAllGenre();
+  res.render('index', {
+    title: 'Browse Genre',
+    content: 'genre',
+    data,
+  });
+};
